0fcce5862ab835f153e9c4e43a7e73a026de5e1f
naman-weybee <naman@weybee.in>
2022-08-31 18:34:32 +0530

    Added MVC to Application

--- WebGentle.BookStore/WebGentle.BookStore/Startup.cs
+++ WebGentle.BookStore/WebGentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace WebGentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,35 +27,63 @@ namespace WebGentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
-           // app.Use(async (context, next) =>
-           //{
-           //    await context.Response.WriteAsync("Hello From My First Middleware \n");
-           //    await next();
-           //    await context.Response.WriteAsync("Hello From My First Middleware Response \n");
-           //});
-
-           // app.Use(async (context, next) =>
-           // {
-           //     await context.Response.WriteAsync("Hello From My Second Middleware \n");
-           //     await next();
-           //     await context.Response.WriteAsync("Hello From My Second Middleware Response \n");
-           // });
-
-           // app.Use(async (context, next) =>
-           // {
-           //     await context.Response.WriteAsync("Hello From My Third Middleware \n");
-           //     await next();
-           // });
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World! \n");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.Map("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
+
+            // app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From My First Middleware \n");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello From My First Middleware Response \n");
+            //});
+
+            // app.Use(async (context, next) =>
+            // {
+            //     await context.Response.WriteAsync("Hello From My Second Middleware \n");
+            //     await next();
+            //     await context.Response.WriteAsync("Hello From My Second Middleware Response \n");
+            // });
+
+            // app.Use(async (context, next) =>
+            // {
+            //     await context.Response.WriteAsync("Hello From My Third Middleware \n");
+            //     await next();
+            // });            
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/", async context =>
+            //    {
+            //        if (env.IsDevelopment())
+            //        {
+            //            await context.Response.WriteAsync("Hello From Development !");
+            //        }
+            //        else if (env.IsProduction())
+            //        {
+            //            await context.Response.WriteAsync("Hello From Production !");
+            //        }
+            //        else if (env.IsStaging())
+            //        {
+            //            await context.Response.WriteAsync("Hello From Staging !");
+            //        }
+            //        else if (env.IsEnvironment("Custom"))
+            //        {
+            //            await context.Response.WriteAsync("Hello From Custom Name !");
+            //        }
+            //        else
+            //        {
+            //            await context.Response.WriteAsync(env.EnvironmentName);
+            //        }
+            //    });
+            //});
         }
     }
 }
